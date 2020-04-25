4cffe2fbe20bdb57ad51ccee8fe7ec180db247f0
Liliana Ester <lili.esterw@gmail.com>
2020-04-25 22:46:42 +1000

    Integrated Dasboard
    Dashboard connected with weather API and events database

--- CaringSquareApp/Controllers/HomeController.cs
+++ CaringSquareApp/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CaringSquareApp.Models;
+using Microsoft.AspNet.Identity;
 
 namespace CaringSquareApp.Controllers
 {
     public class HomeController : Controller
     {
+        private CaringSquareEntities db = new CaringSquareEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,16 @@ namespace CaringSquareApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            var userId = User.Identity.GetUserId();
+            var eventLists = db.SocialEvents.Where(s => s.UserUserId == userId).ToList();
+            //var socialEvents = db.SocialEvents.Include(s => s.AspNetUser).Include(s => s.POIs);
+            return View(eventLists);
         }
     }
 }
